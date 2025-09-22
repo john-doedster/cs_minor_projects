f1768c0c02cb58f78aed3b22897412079766d99c
Ilya Pravilov <ilyapravv@gmail.com>
2025-09-22 21:49:37 +0700

    Update game, add Head for my snake, stop-button esc

--- SnakeEatCavEMPTGame/Form1.Designer.cs
+++ SnakeEatCavEMPTGame/Form1.Designer.cs
@@ -28,15 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1837, 727);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Змея";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- SnakeEatCavEMPTGame/Form1.cs
+++ SnakeEatCavEMPTGame/Form1.cs
@@ -10,21 +10,23 @@ namespace MySnakeGame
         private Timer gameTimer;
         private List<Point> snake;
         private Point food;
-        private int directionX = 1; // 1 = вправо
+        private int directionX = 1;
         private int directionY = 0;
-        private int cellSize = 20;
+        private int cellSize = 50;
         private Random random = new Random();
-        private Image foodImage; // Твоя картинка!
+        private Image foodImage;
+        private bool isPaused = false;
+
 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Змейка с моей картинкой!";
+            this.Text = "Змейка кушает кавчик!";
             this.DoubleBuffered = true; // чтобы не моргало
             this.SetStyle(ControlStyles.ResizeRedraw, true);
 
-            // Загрузи свою картинку
-            foodImage = Image.FromFile("empto.png"); // или Properties.Resources.target если через ресурсы
+            // картинка
+            foodImage = Image.FromFile("empto.png");
 
             StartGame();
         }
@@ -99,7 +101,7 @@ namespace MySnakeGame
         private void GameOver()
         {
             gameTimer.Stop();
-            MessageBox.Show("Game Over! Score: " + (snake.Count - 3));
+            MessageBox.Show("Игра окончена! Результат: " + (snake.Count - 3));
             Application.Restart();
         }
 
@@ -107,13 +109,72 @@ namespace MySnakeGame
         {
             base.OnPaint(e);
 
-            // Рисуем змейку
-            foreach (Point p in snake)
+            // Рисуем тело змейки
+            for (int i = 1; i < snake.Count; i++)
             {
+                Point p = snake[i];
                 e.Graphics.FillRectangle(Brushes.Green, p.X * cellSize, p.Y * cellSize, cellSize, cellSize);
+                e.Graphics.DrawRectangle(Pens.DarkGreen, p.X * cellSize, p.Y * cellSize, cellSize, cellSize);
             }
 
-            // Рисуем ЕДУ — твою картинку!
+            // Рисуем голову змейки с глазами
+            if (snake.Count > 0)
+            {
+                Point head = snake[0];
+
+                // Голова
+                e.Graphics.FillRectangle(Brushes.Crimson, head.X * cellSize, head.Y * cellSize, cellSize, cellSize);
+                e.Graphics.DrawRectangle(Pens.Black, head.X * cellSize, head.Y * cellSize, cellSize, cellSize);
+
+                // Глаза (в зависимости от направления)
+                int eyeSize = cellSize / 4;
+                if (directionX == 1) // Движение вправо
+                {
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + cellSize - eyeSize - 2,
+                        head.Y * cellSize + 2,
+                        eyeSize, eyeSize);
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + cellSize - eyeSize - 2,
+                        head.Y * cellSize + cellSize - eyeSize - 2,
+                        eyeSize, eyeSize);
+                }
+                else if (directionX == -1) // Движение влево
+                {
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + 2,
+                        head.Y * cellSize + 2,
+                        eyeSize, eyeSize);
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + 2,
+                        head.Y * cellSize + cellSize - eyeSize - 2,
+                        eyeSize, eyeSize);
+                }
+                else if (directionY == -1) // Движение вверх
+                {
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + 2,
+                        head.Y * cellSize + 2,
+                        eyeSize, eyeSize);
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + cellSize - eyeSize - 2,
+                        head.Y * cellSize + 2,
+                        eyeSize, eyeSize);
+                }
+                else if (directionY == 1) // Движение вниз
+                {
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + 2,
+                        head.Y * cellSize + cellSize - eyeSize - 2,
+                        eyeSize, eyeSize);
+                    e.Graphics.FillEllipse(Brushes.White,
+                        head.X * cellSize + cellSize - eyeSize - 2,
+                        head.Y * cellSize + cellSize - eyeSize - 2,
+                        eyeSize, eyeSize);
+                }
+            }
+
+            // Рисуем еду
             if (foodImage != null)
             {
                 e.Graphics.DrawImage(
@@ -126,21 +187,44 @@ namespace MySnakeGame
             }
         }
 
+        private void TogglePause()
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                gameTimer.Stop();
+                ShowPauseMessege();
+            }
+            else gameTimer.Start();
+
+            Invalidate();
+        }
+
+        private void ShowPauseMessege()
+        {
+            MessageBox.Show("Игра на паузе. Нажмите ОК, а затем клавишу ESC, чтобы продолжить игру", "ПАУЗА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
             {
                 case Keys.Up:
-                    if (directionY == 0) { directionX = 0; directionY = -1; }
+                    if (directionY == 0 && !isPaused) { directionX = 0; directionY = -1; }
                     break;
                 case Keys.Down:
-                    if (directionY == 0) { directionX = 0; directionY = 1; }
+                    if (directionY == 0 && !isPaused) { directionX = 0; directionY = 1; }
                     break;
                 case Keys.Left:
-                    if (directionX == 0) { directionX = -1; directionY = 0; }
+                    if (directionX == 0 && !isPaused) { directionX = -1; directionY = 0; }
                     break;
                 case Keys.Right:
-                    if (directionX == 0) { directionX = 1; directionY = 0; }
+                    if (directionX == 0 && !isPaused) { directionX = 1; directionY = 0; }
+                    break;
+
+                case Keys.Escape:
+                    TogglePause();
                     break;
             }
         }
@@ -152,7 +236,7 @@ namespace MySnakeGame
                 gameTimer?.Dispose();
                 foodImage?.Dispose();
             }
-            // ВАЖНО: вызвать базовый Dispose, чтобы не сломать форму!
+            //вызвать базовый Dispose, чтобы не сломать форму!
             base.Dispose(disposing);
         }
 
